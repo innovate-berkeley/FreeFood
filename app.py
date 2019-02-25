813cb4b17868c7854b662e952d08efa15d755324
BernardZhao <bernard.zhao.us@gmail.com>
2019-02-24 17:46:36 -0800

    added gitignore

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+from twilio.rest import Client
+
+account_sid = 'AC0f1074a007138b64c7b1b5568afa9486'
+auth_token = 'b01683d2e9a56d5e43d71c3bb6eb7bc1'
+client = Client(account_sid, auth_token)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+@app.route('/test', methods=['POST'])
+def test_message():
+   pass
 
 if __name__ == '__main__':
     app.run(debug=True)
