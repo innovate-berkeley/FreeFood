e6f84da19a1dae9a998f3c0263810a5776c19aac
BernardZhao <bernard.zhao.us@gmail.com>
2019-03-21 00:52:00 -0700

    byebye credentials

--- app.py
+++ app.py
@@ -1,30 +1,46 @@
 from flask import Flask, jsonify, request
 from twilio.rest import Client
-import sys
+import sys, os
 
-account_sid = 'AC0f1074a007138b64c7b1b5568afa9486'
-auth_token = 'b01683d2e9a56d5e43d71c3bb6eb7bc1'
+from dotenv import load_dotenv
+load_dotenv()
+
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("AUTH_TOKEN")
+phone_number = os.getenv("PHONE_NUMBER")
+
 client = Client(account_sid, auth_token)
 
 app = Flask(__name__)
 
+def get_numbers():
+   return ["+17706811621", "+16508620505", "+19168059032"]
+
+for number in get_numbers():
+   validation_request = client.validation_requests \
+                        .create(
+                              friendly_name='',
+                              phone_number=number
+                           )
+   print(validation_request)
+
 @app.route('/')
 def index():
-    return "Hello, World!"
+   return "Hello, World!"
 
 @app.route('/test', methods = ['POST'])
-def test_message():
+def test_mass_message():
    text = request.args.get("message", default = "Yo")
    for number in get_numbers():
       message = client.messages.create(
       to=number, 
-      from_="+12406812052",
+      from_=phone_number,
       body=text)
    return message.sid
 
+def add_number():
+   pass
 
-def get_numbers():
-   return ["+19168059032", "+17706811621", "+16508620505"]
 
 if __name__ == '__main__':
     app.run(debug=True)
